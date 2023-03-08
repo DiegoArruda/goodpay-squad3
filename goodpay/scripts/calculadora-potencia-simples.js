e4425ff88e38a4edbb2489fa50cb145a79c7bfc8
Diego Arruda <arrudadiego1@gmail.com>
2023-03-08 10:13:31 -0300

    Adição de nova calculadora, mudança do nome do arquivo css

--- goodpay/scripts/calculadora-potencia-simples.js
+++ goodpay/scripts/calculadora-potencia-simples.js
@@ -2,15 +2,14 @@ const porcentagemVal1 = document.getElementById("porcentagemVal1");
 const valorNumerico1 = document.getElementById("valorNumerico1");
 const resultado = document.getElementById("resultadoCalc1");
 
-function potenciacao() {
+function porcentagem() {
   let retorno = 0;
   retorno = (valorNumerico1.value / 100) * porcentagemVal1.value;
   resultado.value = `R$${retorno.toFixed(2)}`;
 }
 
-/* function percentual() { */
-/*   var num1 = document.getElementById("").value; */
-/*  */
-/*   var num2 = document.getElementById("").value; */
-/*   document.getElementById("").value = (num1 * 100) / num2 + "%"; */
-/* } */
+function percentual() {
+  var num1 = document.getElementById("valorNumeroVal2").value;
+  var num2 = document.getElementById("valorNumeroVal3").value;
+  document.getElementById("resultadoCalc").value = (num1 * 100) / num2 + "%";
+}
